7dc30f226947b3dcdc19d2c54cdf1bc54713e343
hoa0 <t-hoa.l@hotmail.com>
2021-11-10 10:00:11 +0100

    create circle and rect svg

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,7 @@ function getInfo() {
   }
 
   console.log('test 3',getInfo())*/
+  /*
   console.log('hello JS')
 const URL = `https://www.fruityvice.com/`
 const endpoint = `api/fruit/all`
@@ -30,4 +31,23 @@ function getData() {
     .then(data => console.table(data))
 }
 
-getData()
+getData()*/
+
+d3.json('https://www.fruityvice.com/api/fruit/all').then(x=> console.log(x))
+
+// tutorial 
+const margin = {top: 40, bottom: 10, left: 120, right: 20};
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
+
+// Creates sources <svg> element
+const svg = d3.select('section').append('svg')
+.attr('width', width+margin.left+margin.right)
+.attr('height', height+margin.top+margin.bottom);
+
+  // Group used to enforce margin
+  const g = svg.append('g')
+              .attr('transform', `translate(${margin.left},${margin.top})`);
+
+g.append("circle").attr("r",10).attr("x",9).attr("y",3).style('fill',"blue")
+g.append('rect').attr("height", 20).attr("width",20).style("fill","pink")
